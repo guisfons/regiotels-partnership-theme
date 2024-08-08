748201b06b5b9223733d124f278cd5d830123326
guisfons <guilhermesfonsecaa@gmail.com>
2024-08-08 17:31:40 -0300

    change images url

--- front-page.php
+++ front-page.php
@@ -43,21 +43,21 @@ get_header();
 				<!-- <h4>Our expertise</h4> -->
 
 				<article>
-					<figure><img src="./assets/img/revenue_management.webp" alt="Revenue Management"></figure>
+					<figure><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/revenue_management.webp" alt="Revenue Management"></figure>
 					<h3>Revenue Management</h3>
 					<p>Pricing Strategies, Channel Manager, Booking Engine Setup and Monthly Performance Reports</p>
 					<a href="#contact">Get in touch →</a>
 				</article>
 
 				<article>
-					<figure><img src="./assets/img/online_distribution.webp" alt="Online Distribution"></figure>
+					<figure><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/online_distribution.webp" alt="Online Distribution"></figure>
 					<h3>Online Distribution</h3>
 					<p>OTA Connection and Management, Online Sales and Diversified Booking Channels</p>
 					<a href="#contact">Get in touch →</a>
 				</article>
 
 				<article>
-					<figure><img src="./assets/img/online_distribution.webp" alt="Digital Marketing"></figure>
+					<figure><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/online_distribution.webp" alt="Digital Marketing"></figure>
 					<h3>Digital Marketing</h3>
 					<p>Website Management & SEO for Hotels, Multi-platform Marketing Strategy and Online Advertisement</p>
 					<a href="#contact">Get in touch →</a>
@@ -67,13 +67,13 @@ get_header();
 	</section>
 
 	<section class="partnership">
-		<figure class="partnership__image mobile-only"><img src="./assets/img/partnership_image.webp" alt="How does the partnership work?"></figure>
+		<figure class="partnership__image mobile-only"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/partnership_image.webp" alt="How does the partnership work?"></figure>
 		<div class="partnership__heading">
 			<h3>How does the partnership work?</h3>
 			<p>RegiÔtels works as a part of your team, with a streamlined process that ensures our partnership is intuitive and efficient according to your needs.</p>
 		</div>
 		<div class="partnership__wrapper">
-			<figure class="desktop-only"><img src="./assets/img/partnership_image.webp" alt="How does the partnership work?"></figure>
+			<figure class="desktop-only"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/partnership_image.webp" alt="How does the partnership work?"></figure>
 			<div class="partnership__content">
 				<div class="partnership__item">
 					<span>01</span>
@@ -114,7 +114,7 @@ get_header();
 						<path d="M56.0516 7.84C51.1716 8.16 47.3316 9.59999 44.5316 12.16C41.8116 14.64 40.3716 17.76 40.2116 21.52C41.0116 21.12 41.8116 20.84 42.6116 20.68C43.4116 20.44 44.2116 20.32 45.0116 20.32C48.6916 20.32 51.5716 21.56 53.6516 24.04C55.8116 26.52 56.8916 29.52 56.8916 33.04C56.8916 36.96 55.7316 40.2 53.4116 42.76C51.0916 45.24 47.9316 46.48 43.9316 46.48C40.0916 46.48 36.8516 45.08 34.2116 42.28C31.6516 39.48 30.3716 35.32 30.3716 29.8C30.3716 23 31.4516 17.48 33.6116 13.24C35.8516 9 38.8916 5.88 42.7316 3.87999C46.5716 1.87999 51.0116 0.839996 56.0516 0.759997V7.84ZM25.6916 7.84C20.7316 8.16 16.8916 9.59999 14.1716 12.16C11.4516 14.64 10.0116 17.76 9.85156 21.52C10.5716 21.12 11.3316 20.84 12.1316 20.68C13.0116 20.44 13.8516 20.32 14.6516 20.32C18.3316 20.32 21.2116 21.56 23.2916 24.04C25.3716 26.52 26.4116 29.52 26.4116 33.04C26.4116 36.96 25.2516 40.2 22.9316 42.76C20.6916 45.24 17.5716 46.48 13.5716 46.48C9.73156 46.48 6.49156 45.08 3.85156 42.28C1.29156 39.48 0.0115627 35.32 0.0115627 29.8C0.0115627 23 1.09156 17.48 3.25156 13.24C5.41156 9 8.41156 5.88 12.2516 3.87999C16.1716 1.87999 20.6516 0.839996 25.6916 0.759997V7.84Z" fill="#F44DA8" />
 					</svg></figure>
 				<article>
-					<figure><img src="./assets/img/auvieuxmoulinlogo.webp" alt="Au Vieux Moulin"></figure>
+					<figure><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/auvieuxmoulinlogo.webp" alt="Au Vieux Moulin"></figure>
 					<p><cite>The RegiÔtels team put in place all the actions we discussed and implemented pricing, commercial, and marketing strategies to <strong>improve the turnover of our rooms</strong>, which also had a <strong>positive impact on our restaurant.</strong></cite></p>
 					<pre>— Isabel Boulliat, Au Vieux Moulin</pre>
 				</article>
@@ -131,7 +131,7 @@ get_header();
 						<path d="M56.0516 7.84C51.1716 8.16 47.3316 9.59999 44.5316 12.16C41.8116 14.64 40.3716 17.76 40.2116 21.52C41.0116 21.12 41.8116 20.84 42.6116 20.68C43.4116 20.44 44.2116 20.32 45.0116 20.32C48.6916 20.32 51.5716 21.56 53.6516 24.04C55.8116 26.52 56.8916 29.52 56.8916 33.04C56.8916 36.96 55.7316 40.2 53.4116 42.76C51.0916 45.24 47.9316 46.48 43.9316 46.48C40.0916 46.48 36.8516 45.08 34.2116 42.28C31.6516 39.48 30.3716 35.32 30.3716 29.8C30.3716 23 31.4516 17.48 33.6116 13.24C35.8516 9 38.8916 5.88 42.7316 3.87999C46.5716 1.87999 51.0116 0.839996 56.0516 0.759997V7.84ZM25.6916 7.84C20.7316 8.16 16.8916 9.59999 14.1716 12.16C11.4516 14.64 10.0116 17.76 9.85156 21.52C10.5716 21.12 11.3316 20.84 12.1316 20.68C13.0116 20.44 13.8516 20.32 14.6516 20.32C18.3316 20.32 21.2116 21.56 23.2916 24.04C25.3716 26.52 26.4116 29.52 26.4116 33.04C26.4116 36.96 25.2516 40.2 22.9316 42.76C20.6916 45.24 17.5716 46.48 13.5716 46.48C9.73156 46.48 6.49156 45.08 3.85156 42.28C1.29156 39.48 0.0115627 35.32 0.0115627 29.8C0.0115627 23 1.09156 17.48 3.25156 13.24C5.41156 9 8.41156 5.88 12.2516 3.87999C16.1716 1.87999 20.6516 0.839996 25.6916 0.759997V7.84Z" fill="#F44DA8" />
 					</svg></figure>
 				<article>
-					<figure><img src="./assets/img/landhotelkroliklogo.webp" alt="Landhotel Krolik"></figure>
+					<figure><img src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/landhotelkroliklogo.webp" alt="Landhotel Krolik"></figure>
 					<p><cite>We <strong>saved a lot of our time working with RegiÕtels</strong> because connecting to different OTAs or working with providers from the BeNeLux region was <strong>much easier and faster and much less complicated</strong> than if I had to take care of everything myself.</cite></p>
 					<pre>— Dominik Krolik, Landhotel Krolik</pre>
 				</article>
