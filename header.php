b2a5443d4fc51b8a58e6ccf952046f6d7d730d7b
guisfons <guilhermesfonsecaa@gmail.com>
2024-08-09 12:13:44 -0300

    fixes favicon

--- header.php
+++ header.php
@@ -4,15 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
 	<title><?php echo get_bloginfo('name'); ?></title>
-
-    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/site.webmanifest">
-    <link rel="mask-icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/safari-pinned-tab.svg" color="#5bbad5">
-    <link rel="shortcut icon" href="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/favicon.ico">
     <meta name="msapplication-TileColor" content="#da532c">
-    <meta name="msapplication-config" content="<?php echo esc_url(get_template_directory_uri()); ?>/assets/img/favicon/browserconfig.xml">
     <meta name="theme-color" content="#ffffff">
 
 	<?php
